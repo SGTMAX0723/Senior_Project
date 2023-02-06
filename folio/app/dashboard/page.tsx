cf0056245bde955f8c4e4fffb98109f7a0333f38
Ethan <63824324+ethanvillalobos8@users.noreply.github.com>
2023-02-05 20:23:34 -0600

    Add main tag to hold all subsequent tags

--- folio/app/dashboard/page.tsx
+++ folio/app/dashboard/page.tsx
@@ -3,9 +3,11 @@ import SideBar from "components/SideBar";
 
 export default function Dashboard() {
     return(
-        <div className="bg-primary h-screen" >
-            <NavBar/>
-            <SideBar/>
-        </div>
+        <main>
+            <div className="bg-primary h-screen" >
+                <NavBar/>
+                <SideBar/>
+            </div>
+        </main>
     )
 }
