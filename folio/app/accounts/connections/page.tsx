fef27d2223efbaa861334cfad73b85fa450ca460
Alan Lopez <70230548+Alanl101@users.noreply.github.com>
2023-02-20 18:16:33 -0600

    3rd try

--- folio/app/accounts/connections/page.tsx
+++ folio/app/accounts/connections/page.tsx
@@ -30,16 +30,151 @@ export default function Connections() {
                                 flex
                                 bg-primary'>
     
-                    <div className='container mx-auto my-auto'>
-                        <div className='grid grid-cols-1 gap-16
-                                        md:grid-cols-2 
-                                        lg:grid-cols-2 
-                                        xl:grid-cols-3 
-                                        justify-items-center'>
-                            <h1>Connections Page</h1>
-                        </div>
+                         <div className="container container m-auto flex flex-wrap flex-col md:flex-row items-center justify-center"> 
+        
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Alan Lopez</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: Alan@utrgv.edu <br></br> Github: Alan3832</p>
+                        
+
                     </div>
+
+
                 </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Alan Lopez</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: Alan@utrgv.edu <br></br> Github: Alan3832</p>
+                        
+
+                    </div>
+
+
+                </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Ethan V</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: Ethan.V@utrgv.edu <br></br> Github: Ethan_man</p>
+                        
+
+                    </div>
+
+
+                </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Gabe</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: Gabe@utrgv.edu <br></br> Github: GABE_SGT</p>
+                        
+
+                    </div>
+
+
+                </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Max Million</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: MAX@utrgv.edu <br></br> Github: MAX_SGT</p>
+                        
+
+                    </div>
+
+
+                </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Alan Lopez</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: Alan@utrgv.edu <br></br> Github: Alan3832</p>
+                        
+
+                    </div>
+
+
+                </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Alan Lopez</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: Alan@utrgv.edu <br></br> Github: Alan3832</p>
+                        
+
+                    </div>
+
+
+                </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Ethan V</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: Ethan.V@utrgv.edu <br></br> Github: Ethan_man</p>
+                        
+
+                    </div>
+
+
+                </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Gabe</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: Gabe@utrgv.edu <br></br> Github: GABE_SGT</p>
+                        
+
+                    </div>
+
+
+                </div>
+
+                <div className="w-2/3 lg:w-1/2 p-2 ">
+                    <div className="flex flex-col lg:flex-row rounded h-auto lg:h-32 border bg-white justify-between leading-normal">
+
+                        <img className="h-16 w-16 rounded-full place-self-center lg:ml-4 " 
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                        <div className="text-black font-bold text-l mb-2 leading-tight place-self-center">Max Million</div>
+                        <p className="text-grey-darker place-self-center lg:mr-3 ">Email: MAX@utrgv.edu <br></br> Github: MAX_SGT</p>
+                        
+
+                    </div>
+
+
+                    </div>
+
+                </div>
+            </div>
                 <NavBarLogged />
                 <SideBar />
             </main>
