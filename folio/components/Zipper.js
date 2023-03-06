8a5b5a3d8c94780b93254be356ca1765572f3b20
ggutie53 <89817232+ggutie53@users.noreply.github.com>
2023-03-06 01:12:27 -0600

    Made component into function and added a variable for file

--- folio/components/Zipper.js
+++ folio/components/Zipper.js
@@ -1,13 +1,14 @@
+const Zipper = (file) => {
 var zip = new JSzip();
 
 // Add an top-level, arbitrary text file with contents
 zip.file("Hello.txt", "Hello World\n");
 
 // Generate a directory within the Zip file structure
-var img = zip.folder("css");
+var folio = zip.folder("css");
 
 // Add a file to the directory, in this case an image with data URI as contents
-img.file("smile.gif", imgData, {base64: true});
+folio.file({file}, imgData, {base64: true});
 
 // Generate the zip file asynchronously
 zip.generateAsync({type:"blob"})
@@ -15,3 +16,4 @@ zip.generateAsync({type:"blob"})
     // Force down of the Zip file
     saveAs(content, "archive.zip");
 });
+}
