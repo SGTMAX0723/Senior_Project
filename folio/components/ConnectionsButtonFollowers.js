43a0528affde40dc1d4e07a74bac2a2686145e73
Alanl101 <70230548+Alanl101@users.noreply.github.com>
2023-03-14 21:42:46 -0500

    Little update

--- folio/components/ConnectionsButtonFollowers.js
+++ folio/components/ConnectionsButtonFollowers.js
@@ -7,7 +7,7 @@ function ConnectionsButton() {
     <div className='min-h-screen ml-48 pt-36 pb-16
                                 flex justify-center items-center
                                 bg-primary'>
-      <div className="fixed top-20 h-16 w-screen pl-48 justify-center items-center grid grid-cols-2 bg-primary space-x-2">
+      <div className="fixed top-20 h-14 w-2/6 pl-46 justify-center items-center grid grid-cols-2 bg-primary space-x-2">
           <Link className=' w-32 grid justify-self-end justify-center border rounded bg-white ' href="/accounts/connections">
               <button className='text-black flex'>Following</button>
           </Link>
