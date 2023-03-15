c2bacecc75f5659dd19d6b24d4d0a2f5b405f055
Alanl101 <70230548+Alanl101@users.noreply.github.com>
2023-03-15 18:34:43 -0500

    Updated follow button and following tabs

--- folio/components/ConnectionsButtonFollowing.js
+++ folio/components/ConnectionsButtonFollowing.js
@@ -7,8 +7,8 @@ function ConnectionsButtonFollowing() {
                                 flex justify-center items-center
                                 bg-primary'>
             <div className="fixed top-20 h-14 w-2/6 pl-46 justify-center items-center grid grid-cols-2 bg-primary space-x-2">
-            <Link className=' w-32 grid justify-self-end justify-center rounded bg-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 border border-black' href="/accounts/connections">
-                <button className='text-white flex  '>Following</button>
+            <Link className=' w-32 grid justify-self-end justify-center rounded bg-white bg-indigo-500  border border-gray-500' href="/accounts/connections">
+                <button className='text-white flex font-bold '>Following</button>
             </Link>
             <Link className='border rounded grid justify-center w-32 bg-white ' href="/accounts/followers ">
                 <button className='text-black flex '>Followers</button>
