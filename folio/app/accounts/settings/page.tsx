a05f8bcd1b7c25c5a6284e64b76b7c719d7e2557
Alan Lopez <70230548+Alanl101@users.noreply.github.com>
2023-03-20 16:21:44 -0500

    added photo to settings page

--- folio/app/accounts/settings/page.tsx
+++ folio/app/accounts/settings/page.tsx
@@ -5,22 +5,34 @@ import SideBar from '../../../components/SideBar'
 import NavBarLogged from '../../../components/NavBarLogged'
 import { useEffect, useState } from 'react';
 import  { pb } from 'components/UserAuthentication';
+import Link from 'next/link';
+import UploadPhoto from 'components/UploadPhoto'
 
 export default function Settings() {
-    // Re-renders the component after the first render
-    const [hydrated, setHydrated] = useState(false);
-    useEffect(() => {
-        // This forces a rerender, so the page is rendered
-        // the second time but not the first
-        setHydrated(true);
-    }, []);
-    if (!hydrated) {
-        // Returns null on first render, so the client and server match
-        return null;
-    }
 
     const isLoggedIn = pb.authStore.isValid;
     const router = useRouter();
+
+    const user: any = pb.authStore.model
+
+    //Place Profile picture in a useState 
+    const [profilePicture, setProfilePicture] = useState(`https://folio-database.fly.dev/api/files/_pb_users_auth_/${user.id}/${user.avatar}`)
+
+    //Create a popup window this popup window ill take a file input of an image that will let you change your photo
+    const [buttonPopup, setbuttonPopup] = useState(false);
+
+    
+    const handleUpdatePicture = async (selectedFile: any) => {
+        try {
+            console.log('handleUpdatePicture called'); 
+            const record = await pb.collection('users').update('avatar', selectedFile);
+            console.log('record', record); 
+            return console.log(record);
+        } catch (error){
+            console.error(error);
+            console.log();
+        }
+    }
 
     if (isLoggedIn) {
         return (
@@ -30,45 +42,47 @@ export default function Settings() {
                                     bg-primary'>
         
                         <div className='container mx-auto my-auto'>
-                            <div className='grid grid-cols-1 gap-16 justify-items-center '>
+                            <div className='grid grid-cols-1 gap-2 space-y-4 justify-items-center '>
                                 
-                                <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-3 mt-6'>
+                                <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-3 mt-4'>
                                     <div>
                                         <img className="inline-block h-36 w-36 rounded-full" 
-                                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="https://cdn140.picsart.com/364832977021201.png"/>
+                                        src={profilePicture} />
                                     </div>
-                                    <button className='bg-indigo-900 w-40 h-9 self-center justify-self-center rounded text-white bold'> Upload new photo </button>
-        
+                                    <button onClick={() => setbuttonPopup(true)} className='bg-indigo-900 w-40 h-9 self-center justify-self-center rounded text-white bold'> Upload new photo </button>
+
                                 </div>
-        
-                                <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-3'>
-                                    <input name="firstname" type="firstname" className=" w-48 justify
-                                    appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="First name"></input>
-        
-                                    <input name="lastname" type="Lastname" className=" w-48 flex justify-items-end appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Last name"></input>
+
+                                <UploadPhoto 
+                                    trigger={buttonPopup} 
+                                    setTrigger={setbuttonPopup} 
+                                    profilePicture={profilePicture} 
+                                    setProfilePicture={setProfilePicture} 
+                                    handleUpdatePicture={handleUpdatePicture} 
+                                />
+                                
+                                <div>
+                                    <p>Name</p>
+                                    <input name="name" type="name" className=" md:w-96 sm:w-80 grid justify-items-stretch justify-self-center appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder={user.name}></input>
                                 </div>
-        
-                                <label className="sr-only">Email address</label>
-                                <input name="email" type="email" className=" md:w-96 sm:w-80 grid justify-items-stretch justify-self-center appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address"></input>
+                                        
+                                <div>
+                                    <p>Email</p>
+                                    <p className=" md:w-96 sm:w-80 grid justify-items-stretch justify-self-center appearance-none bg-white rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">{user.email}</p>
+                                </div>
+
+                                <div>
+                                    <p>Github</p>
+                                    <input name="github" type="github" className=" md:w-96 sm:w-80 grid justify-items-stretch justify-self-center appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder={user.githubLink}></input>
+
+                                </div>
         
                                 
-        
-                                <div className='grid md:grid-cols-2 sm:grid-cols-1 sm:gap-4 md:gap-3'>
-        
-                                    <input name="Country" type="Country" className=" w-48 justify
-                                    appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Country"></input>
-        
-                                    <input name="Phonenumber" type="Phonenumber" className=" w-48 flex justify-items-end appearance-none rounded-none rounded-t-md rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Phone number"></input>
-        
-                                    
-        
-        
-                                </div>
-                                
-                                <div className='grid md:grid-cols-2 sm:grid-cols-1 sm:gap-4 md:gap-20'>
-        
+                                <div className='grid md:grid-cols-2 sm:grid-cols-1 sm:gap-4 md:space-x-6'>
+
+                                    <Link href='/accounts/password-reset'>
                                     <button className='bg-orange-400 w-40 h-9 self-center rounded  text-black bold'> Reset password </button>
-        
+                                    </Link>
                                     <button className='bg-indigo-900 w-40 h-9 self-center justify-self-center rounded text-white bold'> Delete account </button>
                                 </div>
                                 
