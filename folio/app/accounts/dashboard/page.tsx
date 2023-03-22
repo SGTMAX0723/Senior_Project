b45764f374e313e46e357e0618cc52c7978aaa20
Ethan <ethan.villalobos8@gmail.com>
2023-03-22 01:55:42 -0500

    Implement dashboard list/grid views

--- folio/app/accounts/dashboard/page.tsx
+++ folio/app/accounts/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import SideBar from '../../../components/SideBar';
 import NavBarLogged from '../../../components/NavBarLogged.js';
 import ImageContainerDashboard from '../../../components/ImageContainerDashboard';
+import ProjectContainerGrid from '../../../components/ProjectContainerGrid';
 import { useEffect, useState } from 'react';
 import  { pb } from 'components/UserAuthentication';
 import { IoIosSearch } from "react-icons/io";
@@ -37,20 +38,61 @@ export default function Dashboard() {
 
         let first_name = user.name.split(' ')[0];
 
+        const [view, setView] = useState(() => {
+            const savedView = localStorage.getItem('view');
+            return savedView || 'list';
+        });
+        useEffect(() => {
+            localStorage.setItem('view', view);
+        }, [view]);
+        
+        const listView = () => {
+            return(
+                <div className='grid grid-cols-1 justify-items-center gap-10'>
+                    {projects.map(({ project_name, project_img, project_url, updated, id }:any, index:number) => {
+                        function loadProject() {
+                            router.push(project_url);
+                        }
+                        return (
+                            <div key={id + view} className='rounded-md w-full max-w-7xl fade-in' style={{animationDelay: `${index * 0.15}s`}}>
+                                <ImageContainerDashboard key={index} project_name={project_name} project_img={project_img} updated={updated} load_project={loadProject} id={id}/>
+                            </div>
+                        );
+                    })}
+                </div>
+            )
+        }
+        const gridView = () => {
+            return(
+                <div className='grid gap-10 min-[0px]:gap-y-10 sm:grid-cols-1 sm:gap-y-10 md:grid-cols-1 md:gap-y-10 lg:grid-cols-2 lg:gap-y-10 xl:grid-cols-3 h-full w-full justify-items-center items-center'>
+                    {projects.map(({ project_name, project_img, project_url, updated, id }:any, index:number) => {
+                            function loadProject() {
+                                router.push(project_url);
+                            }
+                            return (
+                                <div key={id + view} className='w-full max-w-7xl fade-in' style={{animationDelay: `${index * 0.1}s`}}>
+                                    <ProjectContainerGrid key={index} project_name={project_name} project_img={project_img} updated={updated} load_project={loadProject} id={id}/>
+                                </div>
+                            );
+                        })}
+                </div>
+            )
+        }
+
         return (
             <main className='min-h-screen'>
                 <div className='min-h-screen ml-48 pt-36 pb-16
-                                flex justify-center items-center
+                                flex justify-center items-start
                                 bg-primary'>
 
-                    <div className='grid grid-cols-1 w-full mx-16 my-auto h-screen-64'>
+                    <div className='grid grid-cols-1 w-full mx-16 h-screen-64'>
                         <div className='px-2 h-36 max-w-7xl justify-self-center w-full'>
                             <div className='flex flex-row justify-between'>
                                 <h1 className='text-4xl text-secondary font-semi-bold'>Welcome back, <span className="font-light italic">{first_name}</span></h1>
                                 <div className="flex relative text-zinc-500 w-2/5 xl:visible lg:visible md:invisible min-[0px]:invisible max-sm:invisible">
                                     <div className='flex space-x-3 pr-3'>
-                                        <button className='bg-zinc-50 hover:bg-[#A3A0FB] hover:text-zinc-50 rounded-md h-9 w-9 self-center'><BsListUl className='mx-auto my-auto' size={26}/></button>
-                                        <button className='bg-zinc-50 hover:bg-[#A3A0FB] hover:text-zinc-50 rounded-md h-9 w-9 self-center'><BsViewList className='mx-auto my-auto' size={26}/></button>
+                                        <button onClick={() => {setView('list')}} className='bg-zinc-50 hover:bg-[#A3A0FB] hover:text-zinc-50 rounded-md h-9 w-9 self-center'><BsListUl className='mx-auto my-auto' size={26}/></button>
+                                        <button onClick={() => {setView('grid')}} className='bg-zinc-50 hover:bg-[#A3A0FB] hover:text-zinc-50 rounded-md h-9 w-9 self-center'><BsViewList className='mx-auto my-auto' size={26}/></button>
                                     </div>
                                     <input
                                         className="bg-zinc-50 h-10 w-full pl-4 rounded-md text-sm font-light focus:outline-none tracking-[.07em]"
@@ -64,18 +106,7 @@ export default function Dashboard() {
                                 </div>
                             </div>
                         </div>
-                        <div className='grid grid-cols-1 justify-items-center gap-10'>
-                            {projects.map(({ project_name, project_img, project_url, updated, id }:any, index:number) => {
-                                function loadProject() {
-                                    router.push(project_url);
-                                }
-                                return (
-                                    <div className='rounded-md w-full max-w-7xl'>
-                                        <ImageContainerDashboard key={index} project_name={project_name} project_img={project_img} updated={updated} load_project={loadProject} id={id}/>
-                                    </div>
-                                );
-                            })}
-                        </div>
+                        {view === 'list' ? listView() : gridView()}
                     </div>
                 </div>
                 <NavBarLogged />
---
+++ folio/components/ProjectContainerGrid.js
@@ -0,0 +1,95 @@
+'use client'
+
+import { BsThreeDots } from "react-icons/bs";
+import { useState, useEffect, useRef } from 'react';
+import ProjectDropdown from './ProjectDropdown';
+
+const ProjectContainerGrid = (props) => {
+    const [isIconClicked, setIsIconClicked] = useState(false);
+    const projDropdownRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (isIconClicked && !projDropdownRef.current.contains(event.target)) {
+                setIsIconClicked(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isIconClicked]);
+
+    const handleIconClick = () => {
+        setIsIconClicked(!isIconClicked);
+    };
+
+    const lastUpdatedDate = new Date(props.updated);
+    const currentDate = new Date();
+    const timeDiff = currentDate.getTime() - lastUpdatedDate.getTime(); // get the difference in milliseconds
+
+    const oneDay = 24 * 60 * 60 * 1000; // one day in milliseconds
+    const oneMonth = 30 * oneDay; // one month in milliseconds
+    const oneYear = 12 * oneMonth; // one year in milliseconds
+
+    let diffText;
+    if (timeDiff < oneDay) {
+        diffText = "Today";
+    } else if (timeDiff < 2 * oneDay) {
+        diffText = "Yesterday";
+    } else if (timeDiff < oneMonth) {
+        diffText = `${Math.floor(timeDiff / oneDay)} days ago`;
+    } else if (timeDiff < 2 * oneMonth) {
+        diffText = "1 month ago";
+    } else if (timeDiff < oneYear) {
+        diffText = `${Math.floor(timeDiff / oneMonth)} months ago`;
+    } else {
+        diffText = `${Math.floor(timeDiff / oneYear)} years ago`;
+    }
+
+    const [showButton, setShowButton] = useState(false);
+
+    return (
+        <main className="w-full max-w-7xl rounded-md">
+            <div className="flex flex-col h-60 rounded-md bg-zinc-50 drop-shadow-md">
+                <div className="h-3/5 w-full overflow-hidden overflow-y-auto rounded-t-md">
+                    {/* <img src={`https://folio-database.fly.dev/api/files/zncdy0gkg9xd30m/${props.id}/${props.project_img}`}/> */}
+                    <div className="flex w-full h-full bg-gradient-to-r from-[#A3A0FB] to-[#E53F71] justify-center items-center"
+                        onMouseEnter={() => setShowButton(true)}
+                        onMouseLeave={() => setShowButton(false)}>
+                        {showButton && (
+                            <button onClick={props.load_project} className="bg-secondary hover:brightness-150 text-white py-2 px-4 rounded-md">
+                                Load Project
+                            </button>
+                        )}
+                    </div>
+                </div>
+                <div className="grid grid-col-2 h-2/5">
+                    <div>
+                        <div className="grid grid-cols-2 w-full h-full">
+                            <button className="w-full items-center justify-start pl-4" onClick={props.load_project}>
+                                <div className="flex w-full text-xl hover:text-[#A3A0FB] font-medium">
+                                    <p className="truncate">{props.project_name}</p>
+                                </div>
+                            </button>
+                            <p className="flex w-full items-center justify-end pr-6 text-sm text-gray-600">Blank Template</p>
+                        </div>
+                    </div>
+                    <div>
+                        <button onClick={handleIconClick} className="absolute bottom-0 right-0 pb-4 pr-6" ref={projDropdownRef}>
+                            <BsThreeDots className="text-lg" />
+                            {isIconClicked && (
+                                <div className="dropdown-menu">
+                                    <ProjectDropdown id={props.id} />
+                                </div>
+                            )}
+                        </button>
+                        <p className="text-sm text-gray-600 absolute bottom-0 left-0 pb-4 pl-4">{diffText}</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default ProjectContainerGrid;
