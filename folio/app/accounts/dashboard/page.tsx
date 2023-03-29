f60eaaf60e50a3b36c99f77f2d5c4ac220ec5bc9
Alanl101 <70230548+Alanl101@users.noreply.github.com>
2023-03-29 17:25:11 -0500

    Worked on dashboard

--- folio/app/accounts/dashboard/page.tsx
+++ folio/app/accounts/dashboard/page.tsx
@@ -10,6 +10,9 @@ import { useEffect, useState } from 'react';
 import  { pb } from 'components/UserAuthentication';
 import { BsViewList } from "react-icons/bs";
 import { BsListUl } from "react-icons/bs";
+import { AiFillMail } from "react-icons/ai";
+import { SiGithub } from "react-icons/si"
+import {ReactNode} from 'react';
 
 export default function Dashboard() {
     const isLoggedIn = pb.authStore.isValid;
@@ -81,14 +84,14 @@ export default function Dashboard() {
 
         return (
             <main className='min-h-screen'>
-                <div className='min-h-screen ml-48 pt-36 pb-16
+                <div className='min-h-screen ml-48 pt-[70px] pb-16
                                 flex justify-center items-start
                                 bg-primary'>
 
                     <div className='grid grid-cols-1 w-full mx-16 h-screen-64'>
-                        <div className='px-2 h-36 max-w-7xl justify-self-center w-full'>
-                            <div className='flex flex-row justify-between'>
-                                <h1 className='text-4xl text-secondary font-semi-bold'>Welcome back, <span className="font-light italic">{first_name}</span></h1>
+                        <div className='px-2 h-36 max-w-7xl justify-self-center w-full '>
+                            <div className='flex flex-row justify-between '>
+                                <h1 className='text-4xl text-secondary font-semi-bold invisible md:visible'>Welcome back, <span className="font-light italic visible relative absolute -top-[30px] md:top-0 ">{first_name}</span></h1>
                                 <div className="flex relative text-zinc-500 w-2/5 xl:visible lg:visible md:invisible min-[0px]:invisible max-sm:invisible">
                                     <div className='flex space-x-3 pr-3 items-start justify-center'>
                                         <button onClick={() => {setView('list')}} className='bg-zinc-50 hover:bg-[#A3A0FB] hover:text-zinc-50 rounded-md h-10 w-10'><BsListUl className='mx-auto my-auto' size={26}/></button>
@@ -97,8 +100,27 @@ export default function Dashboard() {
                                     <SearchBarDashboard />
                                 </div>
                             </div>
+
+
+                            <div className='border grid grid-cols-1 md:grid-cols-2 w-full relative absolute -top-[30px] md:top-1'>
+
+                                <img className="inline-block h-36 w-36 rounded-full ml-6" 
+                                        src={`https://folio-database.fly.dev/api/files/_pb_users_auth_/${user.id}/${user.avatar}`} />
+                                
+                                <div className='invisible md:visible '>         
+                                    <div className="text-black text-l mb-2 leading-tight place-self-center">This is my bio. These bios will have a character limit. The charcter limit will be the size of the text. Haven't reached the limit of anything until now 
+                                    </div> 
+                                     <div>
+                                        <IconText icon={<AiFillMail size='20' />} text={<p className="text-grey-darker lg:mr-3 " >{user.email}</p>}/>
+                                        <IconText icon={<SiGithub size='20' />} text={<a className="text-blue-500 mr-3" href={user.githubLink}>{user.githubLink} </a>}/>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
                         </div>
-                        <div className='w-full h-full z-0'>
+                        <div className='w-full h-full z-0 relative absolute top-[120px] '>
                             {view === 'list' ? listView() : gridView()}
                         </div>
                     </div>
@@ -111,3 +133,17 @@ export default function Dashboard() {
         router.push('/login');
     }
 }
+
+interface IconTextProps {
+    icon: ReactNode;
+    text: ReactNode;
+  }
+  
+  
+  const IconText = ({ icon, text }: IconTextProps) => (
+    <div className='icon-text flex items-center space-x-2 ml-1 mr-3'>
+      {icon}
+      {text}
+    </div>
+  );
+  
